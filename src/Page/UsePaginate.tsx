29c4595d48b54e5fcd42fbb02c27a77d457ef69e
felipe <felipekurosaki123@gmail.com>
2022-07-21 16:22:54 -0300

    first commit

---
+++ src/Page/UsePaginate.tsx
@@ -0,0 +1,13 @@
+import './usePaginate.scss'
+export default function UsePaginate(){
+return(
+    <>
+    <div className="cotainer">
+        <h1>Desafio <span>Paginação</span><small>Custom Hooks</small></h1>
+        <div className="paginateBody">
+            
+        </div>
+    </div>
+    </>
+)
+}
